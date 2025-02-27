60469e2bd2883662d7c17e22e86dc7ac6e06e40a
Shubhanjan-GGOD <bera.shubhanjan@gmail.com>
2025-02-27 11:24:32 +0530

    Add Authorization to our API

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,22 +1,25 @@
 using System;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
-[ApiController]
-[Route("api/[controller]")]// /api/users
-public class UsersController(DataContext context) : ControllerBase
+// [ApiController]
+// [Route("api/[controller]")]// /api/users
+
+public class UsersController(DataContext context) : BaseApiController
 {
     //private readonly DataContext _context = context;
-
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         var users = await context.Users.ToListAsync();
         return Ok(users);
     }
+    [Authorize]
      [HttpGet("{id:int}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,39 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt=>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddCors();
+// builder.Services.AddControllers();
+// builder.Services.AddDbContext<DataContext>(opt=>
+// {
+//     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+// });
+// builder.Services.AddCors();
+// builder.Services.AddScoped<ITokenService,TokenService>();
+builder.Services.AddApplicationServices(builder.Configuration);
+// builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+// .AddJwtBearer(Options=>
+// {
+//     var tokenKey =builder.Configuration["TokenKey"]??throw new Exception("Token Key not Found");
+//     Options.TokenValidationParameters = new TokenValidationParameters
+//     {
+//         ValidateIssuerSigningKey =true,
+//         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+//         ValidateIssuer =false,
+//         ValidateAudience = false
+//     };
+// }
+// );
+builder.Services.AddIdentityServices(builder.Configuration);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 // builder.Services.AddEndpointsApiExplorer();
 // builder.Services.AddSwaggerGen();
@@ -28,6 +51,8 @@ var app = builder.Build();
 
 // app.UseAuthorization();
 app.UseCors(x =>x.AllowAnyHeader().AllowAnyHeader().WithOrigins("http://localhost:4200","http://localhost:4200"));
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
